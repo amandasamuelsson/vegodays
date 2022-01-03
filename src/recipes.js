159b2330e864d44c3d8a8694fffe4077c5ca1d04
Malin Österberg <msmalinosterberg@gmail.com>
2022-01-03 13:18:08 +0100

    img link on all recipes

--- src/recipes.js
+++ src/recipes.js
@@ -2,30 +2,34 @@ export const recipesList = [
   {
     id: 1,
     title: "Het nudelwok med grönsaker",
+    img: "https://github.com/amandasamuelsson/vegodays/blob/master/assets/nudlar.jpg?raw=true",
     ingredients:
-      "1 förp äggnudlar. 1 tsk sesamolja. 150 g edamamebönor 2 morötter. 0,5 röd paprika.0,5 röd chili.1 klyfta vitlök.1 msk mushroom soja.10 cm purjolök.2 msk woksås. salt &peppar. En kruka koriander. 2 msk olja att steka i ",
+      "1 förp äggnudlar. 1 tsk sesamolja. 150 g edamamebönor. 2 morötter. 0.5 röd paprika. 0.5 röd chili. 1 klyfta vitlök. 1 msk mushroom soja. 10 cm purjolök. 2 msk woksås. salt &peppar. En kruka koriander. 2 msk olja att steka i",
     instructions:
-      "Koka nudlar 1 minut kortare än anvisningar på förpackningen. Kyl ner i iskallt vatten och låt droppa av väl i durkslag eller sil.Blanda ihop ingredienserna till dressingen. Skär den vita delen av salladslök i 3 cm bitar. Skiva den gröna delen i tunna skivor och ställ åt sidan. Skär paprika i munsbitsstora bitar. Finhacka chilin och vitlöken. Finhacka korianderstjälkar och grovhacka korianderblad. Riv morötter alt. skär mindre stavar. Hetta upp en stor stekpanna eller wok tills den ryker. Häll i lite olja och fräs vitlök, chili och salladslök i någon minut. Ha i resterande grönsaker och fräs någon minut. Vänd ner edamame och sesamfrön, låt fräsa med i en minut. Lägg sedan i de kalla nudlarna i pannan så de får steka varma. Häll över dressingen och vänd runt ordentligt så dressingen täcker nudlar och grönsaker. Strö över korianderhack och servera.",
+      "Koka nudlar 1 minut kortare än anvisningar på förpackningen. Kyl ner i iskallt vatten och låt droppa av väl i durkslag eller sil. Blanda ihop ingredienserna till dressingen. Skär den vita delen av salladslök i 3 cm bitar. Skiva den gröna delen i tunna skivor och ställ åt sidan. Skär paprika i munsbitsstora bitar. Finhacka chilin och vitlöken. Finhacka korianderstjälkar och grovhacka korianderblad. Riv morötter alt. skär mindre stavar. Hetta upp en stor stekpanna eller wok tills den ryker. Häll i lite olja och fräs vitlök, chili och salladslök i någon minut. Ha i resterande grönsaker och fräs någon minut. Vänd ner edamame och sesamfrön, låt fräsa med i en minut. Lägg sedan i de kalla nudlarna i pannan så de får steka varma. Häll över dressingen och vänd runt ordentligt så dressingen täcker nudlar och grönsaker. Strö över korianderhack och servera.",
   },
   {
     id: 2,
     title: "Färgglad pizza",
+    img: "https://github.com/amandasamuelsson/vegodays/blob/master/assets/pizza.jpg?raw=true",
     ingredients:
-      "Pizzadeg. 0.5 paket jäst, färsk. 2.5 dl vatten, fingervarmt. 6 dl vetemjöl. 2 msk olivolja. 0.5 tsk salt. 1 dl vetemjöl, till utbakning. ",
+      "Pizzadeg: 0.5 paket jäst. 2.5 dl vatten. 6 dl vetemjöl. 2 msk olivolja. 0.5 tsk salt. 1 dl vetemjöl, till utbakning.",
     instructions:
       "Pizzadeg: Smula jäst i en bunke och lös upp med fingervarmt vatten. Tillsätt mjöl och olivolja och blanda runt. Knåda degen för hand i 15 minuter eller med maskin i tio minuter så att glutentrådarna utvecklas. Häll i saltet precis i slutet av knådningen. Låt degen jäsa till dubbel storlek under kökshandduk, cirka 30 minuter. Tomatsås: Blanda alla ingredienser till tomatsåsen i en kastrull och låt småputtra i 15 minuter. När såsen puttrat klart fiskar du upp vitlöksklyftorna och chilin om du använt en hel och slänger dem. Smaka av med salt och peppar. Skiva rödlök, paprika och purjolök. Skär de soltorkade tomaterna i mindre bitar. Dela mozzarellan i mindre bitar och riv osten. Sätt ugnen på 275 grader eller högsta under- och övervärme. Dela degen i fyra bitar och dra eller kavla ut den till pizzabottnar på mjölat bakbord. Eller kavla ut en stor pizzabotten till långpannan. Lägg pizzabottnarna på plåtar med bakplåtspapper. Bred på tomatsåsen. Fördela spenat, rödlök, paprika, purjolök och soltorkade tomater jämt över bottnarna. Strö över riven ost. Vänta med mozzarellan. Grädda pizzorna eller långpannepizzan 10–15 minuter tills de fått fin färg och det doftar ljuvligt. Lägg på mozzarellan när det är cirka fem minuter kvar av gräddningstiden. Strö på oregano och vrid ett par varv med svartpepparkvarnen strax innan servering.",
   },
   {
     id: 3,
     title: "Rostad het pumpasoppa",
+    img: "https://github.com/amandasamuelsson/vegodays/blob/master/assets/pumpa.jpg?raw=true",
     ingredients:
-      "ca 1 kg butternutpumpa. 1 gul lök.2 vitlöksklyftor. 2 msk smör. 1 tsk salt. 5 dl vatten. 2 msk grönsaksfond. 2 dl smaksatt creme fraiche paprika och chili. 1.5 msk citronsaft. Pumpafrön",
+      "ca 1 kg butternutpumpa. 1 gul lök. 2 vitlöksklyftor. 2 msk smör. 1 tsk salt. 5 dl vatten. 2 msk grönsaksfond. 2 dl smaksatt creme fraiche paprika och chili. 1.5 msk citronsaft. Pumpafrön",
     instructions:
       "Sätt ugnen på 225°.Halvera pumpan och skrapa ur pumpakärnorna. Skala och skär pumpan i ca 2 cm stora bitar.Skala och finhacka lök och vitlök. Lägg allt på en plåt med bakplåtspapper. Smält smör och ringla över. Strö över hälften av saltet. Rosta i mitten av ugnen ca 20 min tills pumpan är mjuk. Häll över allt i en stor kastrull. Tillsätt vatten, fond och crème fraiche.Koka upp och mixa slätt. Tillsätt resten av saltet och citronsaften. Späd ev soppan med vatten om den blir för tjock. Rosta pumpafrön och toppa soppan med. Smula även över fetaost om du önskar. ",
   },
   {
     id: 4,
     title: "Halloumi Tikka Masala",
+    img: "https://github.com/amandasamuelsson/vegodays/blob/master/assets/tikkamasala.jpg?raw=true",
     ingredients:
       "200 g morötter, grovrivna. cashewnötter. 0.5 lime, pressad saft. 200 g halloumi, tärnad. 1 tsk neutral olja. 1 grönsaksbuljongtärning. 2 dl crème fraiche. koriander. 2 dl vatten. 3 msk tomatpuré. 3 tsk garam masala. 1 msk finhackad ingefära. 2 tsk neutral olja. 1 tsk chiliflakes. 3 vitlöksklyftor, finhackade.1 gul lök, hackad. 400 g krossade tomater. Till servering: 3 dl jasminris",
     instructions:
@@ -34,6 +38,7 @@ export const recipesList = [
   {
     id: 5,
     title: "Het tomatsoppa",
+    img: "https://github.com/amandasamuelsson/vegodays/blob/master/assets/tomatsoppa.jpg?raw=true",
     ingredients:
       "0.5 dl olivolja. 1 burk skalade tomater 400 g. 2 st kvisttomater. 1 st grönsaksbuljongtärning. 0.5 tsk salt. 0.5 tsk socker. 1 st vitlöksklyftor. 0.5 msk balsamvinäger. 1 tsk sambal oelek. 1 dl matlagningsgrädde",
     instructions:
@@ -41,7 +46,8 @@ export const recipesList = [
   },
   {
     id: 6,
-    title: "  Vegetarisk lasagne",
+    title: "Vegetarisk lasagne",
+    img: "https://github.com/amandasamuelsson/vegodays/blob/master/assets/lasagne.jpg?raw=true",
     ingredients:
       " 1 st gul lök. 0.5 tsk salt. 0.5 tsk torkad timjan. 0.5 tsk torkad basilika. 1 tärning grönsaksbuljong. 4 dl krossade tomater. 1 dl vatten. 0.5 tsk svartpeppar. 8 st lasagneplattor, (ca. 1/2 paket). 250g sojafärs. 1 msk olivolja. 1 klyfta vitlök. 1 msk tomatpuré. Ostsås: 1,5 dl riven ost. 1 krm vitpeppar. 1 msk mat- och baksmör. 0.5 dl vetemjöl. 6 dl standardmjölk. 1 tsk salt",
     instructions:
@@ -50,6 +56,7 @@ export const recipesList = [
   {
     id: 7,
     title: "Svampramen med shitake och ostronskivling",
+    img: "https://github.com/amandasamuelsson/vegodays/blob/master/assets/ramen.jpg?raw=true",
     ingredients:
       "Svampbuljong: 0.5 liten gul lök. 0.5 liten morot. 0.5 vitlöksklyfta. 1/4 förp torkad karljohanssvamp (1/2 förp motsvarar 30 g) 0.5 l svampbuljong. 0.5 förp ostronskivling (à 150 g). olja till fritering. 0.5 förp shiitakesvamp (à 150 g). 0.5 msk färskriven ingefära. 1 vitlöksklyfta. 1 msk rapsolja. 0.5 tsk sesamolja. 0.5 msk japansk soja. Topping: 1 salladslökar. 1/2 spansk peppar. 2 port nudlar (gärna udon). 1 ägg ",
     instructions:
@@ -58,14 +65,16 @@ export const recipesList = [
   {
     id: 8,
     title: "Krämig pasta med valnötter",
+    img: "https://github.com/amandasamuelsson/vegodays/blob/master/assets/pasta.jpg?raw=true",
     ingredients:
-      "400 gram Pappardelle. 2.5 dl Grädde .170 g Valnötskärnor . 100 g  Hårdost (lagrad). 0.5 kruka Persilja. 0.5 kruka Basilika. 2 klyfta Vitlök. 1.5 dl Ströbröd. 75 gram Smör. 1 dl Olivolja",
+      "400 gram Pappardelle. 2.5 dl Grädde. 170 g Valnötskärnor. 100 g Hårdost (lagrad). 0.5 kruka Persilja. 0.5 kruka Basilika. 2 klyfta Vitlök. 1.5 dl Ströbröd. 75 gram Smör. 1 dl Olivolja",
     instructions:
       "Koka upp pastavattnet. Häll upp grädde och ströbröd i en skål. Låt stå och svälla.Riv ost och rosta valnötter i en torr panna. Mixa ströbröd, hälften av nötterna, vitlök, persilja, salt och peppar i en mixer. Tillsätt olivolja i en tunn stråle medan mixern går. Tillsätt hälften av osten och basilika. Mixa lite till. Koka pasta al dente. Häll bort vatten i durkslag. Häll tillbaka i kastrullen. Ställ tillbaka på spisen. Lägg i smör och låt det smälta. Tillsätt sås och rör om.  Lägg upp och strö över resten av osten och nötterna. Servera genast.",
   },
   {
     id: 9,
     title: "Svamptacos med spetskål och grön tahinisås",
+    img: "https://github.com/amandasamuelsson/vegodays/blob/master/assets/taco.jpg?raw=true",
     ingredients:
       "150 g blandad svamp (t ex ostronskivling, portabello och champinjon). 1stor rödlök. 0.5 dl rapsolja. 0.5 dl japansk soja. 0.5 tsk torkad timjan. 0.5 krm svartpeppar. Grön tahinisås: 0.5 kruka koriander. 0.5 kruka bladpersilja. 0.5 riven vitlöksklyfta. 0.5 dl ljus tahini. 1 msk färskpressad limejuice. 1 tsk strösocker. 1 krm salt. 0.5 dl vatten. Tillbehör: 125 g spetskål. 0.5 lime. 6 små tortillabröd. 0.5 dl rostade pumpakärnor",
     instructions:
@@ -74,6 +83,7 @@ export const recipesList = [
   {
     id: 10,
     title: "Bönburgare med mangosalsa",
+    img: "https://github.com/amandasamuelsson/vegodays/blob/master/assets/burgare.jpg?raw=true",
     ingredients:
       "0.5 lök. 1 vitlöksklyftor. 1 morötter. 1 förp kokta kidneybönor (à 380 g). 0.5 tsk salt. 0.5 krm peppar. 1 tsk curry. 1 tsk malen koriander. 1,5 tsk chilipulver. 1 ägg. 1 msk majsstärkelse (Maizena). 1,5 msk rapsolja. Mangosalsa: 0.5 liten mogen mango. 1 tomater. 1 röd chilipeppar. 0.5 msk hackad rödlök. 0.5 msk saft av pressad lime. 1 tsk socker. ev färsk koriander",
     instructions:
@@ -82,6 +92,7 @@ export const recipesList = [
   {
     id: 11,
     title: "Vegetariska vårrullar",
+    img: "https://github.com/amandasamuelsson/vegodays/blob/master/assets/springrolls.jpg?raw=true",
     ingredients:
       "50 g Glasnudlar. 12 ark Rispapper. 1 morot. 0.5 Purjolök.  0.5 Gurka. 100 g rödkål. 0.5 kruka Koriander. 2 msk Cashewnötter. 1 dl Smetana. 1 msk Kinesisk soja. 2 msk Strösocker. Saft från 2 Lime. 1 röd chili. 1 vitlök. ",
     instructions:
