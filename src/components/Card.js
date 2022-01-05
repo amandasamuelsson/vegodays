8241c0247963e35015cc72ce8cbfe83ac4eb49dc
Malin Österberg <msmalinosterberg@gmail.com>
2022-01-05 12:30:24 +0100

    added id

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,100 +1,84 @@
-import { Card, CardTitle, CardContent, CardAction, CardButton, CardImage } from 'react-native-cards';
 import {
-    ScrollView,
-    StyleSheet,
-    ActivityIndicator,
-    View
-  } from "react-native";
+  Card,
+  CardTitle,
+  CardContent,
+  CardAction,
+  CardButton,
+  CardImage,
+} from "react-native-cards";
+import { ScrollView, StyleSheet, ActivityIndicator, View } from "react-native";
 
-import React from 'react';
+import React from "react";
 
 export default class RecCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      dataSource: null,
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            isLoading: true,
-            dataSource: null,
-        }
-    }
-
-    componentDidMount () {
-
-        return fetch ('https://amandasamuelsson.github.io/recipes/recipes.json')
-            .then ( (response) => response.json() )
-            .then ( (responseJson) => {
-
-                this.setState({
-                    isLoading: false,
-                    dataSource: responseJson.recipes,
-                })
-
-            })
-
-        .catch((error) => {
-            console.log(error)
+  componentDidMount() {
+    return fetch("https://amandasamuelsson.github.io/recipes/recipes.json")
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson.recipes,
         });
+      })
 
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return (
+        <View style={styles.container}>
+          <ActivityIndicator />
+        </View>
+      );
+    } else {
+      let recipes = this.state.dataSource.map((item, id) => {
+        return (
+          <ScrollView key={id} horizontal={true} style={styles.cardView}>
+            <Card style={styles.card}>
+              <CardImage source={{ uri: item.img }} style={styles.cardImg} />
+              <CardTitle title={item.title} style={styles.cardTitle} />
+
+              <CardContent text="Måndag" />
+              <CardAction
+                //  separator={true}
+                inColumn={false}
+              >
+                <CardButton
+                  onPress={() => {}}
+                  title="★ Favoritmarkera"
+                  color="#FEB553"
+                />
+              </CardAction>
+            </Card>
+          </ScrollView>
+        );
+      });
+      return <View style={styles.container}>{recipes}</View>;
     }
-
-    render() {
-
-        if (this.state.isLoading) {
-            return (
-                <View style={styles.container}>
-                    <ActivityIndicator />
-                </View>
-            )
-        } else {
-
-            let recipes = this.state.dataSource.map((item, key) => {
-                return <ScrollView 
-                key={key}
-                horizontal={true}
-                style={styles.cardView}
-                > 
-                <Card style={styles.card}>
-                  <CardImage 
-                    source={{uri: item.img}} 
-                    style={styles.cardImg}
-                  />
-                  <CardTitle
-                    title={item.title}
-                    style={styles.cardTitle}/>
-               
-                
-                  <CardContent text="Måndag" />
-                  <CardAction 
-                   //  separator={true} 
-                    inColumn={false}>     
-                    <CardButton
-                      onPress={() => {}}
-                      title="★ Favoritmarkera"       
-                      color="#FEB553"
-                    />
-                  </CardAction>
-                </Card>
-               </ScrollView>
-            });
-            return (
-              <View style={styles.container}>
-                  {recipes}
-              </View>
-            );
-          }
-        }
-      }
+  }
+}
 const styles = StyleSheet.create({
-    container: {
-      flexDirection: 'row',
-    },
-    cardTitle: {
-      fontSize: 38,
-    },
-    card: {
-      width: 350,
-    },
-    cardImg: {
-      backgroundColor: '#fff',
-    },
-});  
+  container: {
+    flexDirection: "row",
+  },
+  cardTitle: {
+    fontSize: 38,
+  },
+  card: {
+    width: 350,
+  },
+  cardImg: {
+    backgroundColor: "#fff",
+  },
+});
