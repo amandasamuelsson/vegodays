7dca5090ebd66edc2664f82a8aecc6359b597121
Malin Österberg <msmalinosterberg@gmail.com>
2022-01-11 11:45:48 +0100

    asyncstorage for weekdays

--- src/components/WeekdayPicker.js
+++ src/components/WeekdayPicker.js
@@ -1,97 +1,118 @@
-import * as React from 'react';
+import * as React from "react";
 import {
   View,
   StyleSheet,
   FlatList,
   TouchableOpacity,
-} from 'react-native';
-import Constants from 'expo-constants';
+  Text,
+} from "react-native";
+import Constants from "expo-constants";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Data = [
   {
     id: 1,
-    day_name: 'Måndag',
+    day_name: "Måndag",
   },
   {
     id: 2,
-    day_name: 'Tisdag',
+    day_name: "Tisdag",
   },
   {
     id: 3,
-    day_name: 'Onsdag',
+    day_name: "Onsdag",
   },
   {
     id: 4,
-    day_name: 'Torsdag',
+    day_name: "Torsdag",
   },
   {
     id: 5,
-    day_name: 'Fredag',
+    day_name: "Fredag",
   },
   {
     id: 6,
-    day_name: 'Lördag',
+    day_name: "Lördag",
   },
   {
     id: 7,
-    day_name: 'Söndag',
+    day_name: "Söndag",
   },
 ];
 
-import { Card, CardTitle } from 'react-native-cards';
+import { Card, CardTitle } from "react-native-cards";
 
 export default class DayPicker extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       selectedItem: null,
-      renderData:Data
+      renderData: Data,
     };
-  } 
+  }
+
+  saveData = async () => {
+    let name = "Michal";
+    AsyncStorage.setItem("user", name);
+  };
+
+  displayData = async () => {
+    try {
+      let user = await AsyncStorage.getItem("user");
+      alert(user);
+    } catch (error) {
+      alert(error);
+    }
+  };
 
   onPressHandler(id) {
-    let renderData=[...this.state.renderData];
-    for(let data of renderData){
-      if(data.id==id){
-        data.selected=(data.selected==null)?true:!data.selected;
+    let renderData = [...this.state.renderData];
+    for (let data of renderData) {
+      if (data.id == id) {
+        data.selected = data.selected == null ? true : !data.selected;
+        AsyncStorage.setItem("Data", JSON.stringify(data.selected));
+
         break;
       }
     }
-    this.setState({renderData});
+    this.setState({ renderData });
   }
 
   render() {
-    
     return (
       <View>
+        <TouchableOpacity onPress={this.saveData}>
+          <Text>Click to save data</Text>
+        </TouchableOpacity>
+
         <FlatList
           //horizontal={true}
           style={styles.list}
           data={this.state.renderData}
-          keyExtractor={item => item.id.toString()}
+          keyExtractor={(item) => item.id.toString()}
           showsHorizontalScrollIndicator={false}
           renderItem={({ item }) => (
-              <TouchableOpacity onPress={() => this.onPressHandler(item.id)}>
+            <TouchableOpacity onPress={() => this.onPressHandler(item.id)}>
               <Card
                 style={
-                    item.selected==true
+                  item.selected == true
                     ? {
                         padding: 10,
                         borderRadius: 5,
-                        backgroundColor: '#508268',
-                        color: "#ffffff"
-                    }
+                        backgroundColor: "#508268",
+                        color: "#ffffff",
+                      }
                     : {
                         padding: 10,
                         borderRadius: 5,
-                        backgroundColor: '#e3e3e3',
-                    }
-                }>
+                        backgroundColor: "#e3e3e3",
+                      }
+                }
+              >
                 <CardTitle
-                style={styles.cardText}
-                title= {item.day_name}
-                >
-                </CardTitle>
+                  style={styles.cardText}
+                  title={item.day_name}
+                ></CardTitle>
               </Card>
             </TouchableOpacity>
           )}
@@ -102,15 +123,15 @@ export default class DayPicker extends React.Component {
 }
 
 const styles = StyleSheet.create({
-    list: {
-        width: 300,
-    },
-    cardText: {
-        flexDirection: "column",
-        color: "#fff",
-        textAlign: "center",
-        alignItems: "center",
-        alignSelf: "center",
-        justifyContent: "center",
-    },
+  list: {
+    width: 300,
+  },
+  cardText: {
+    flexDirection: "column",
+    color: "#fff",
+    textAlign: "center",
+    alignItems: "center",
+    alignSelf: "center",
+    justifyContent: "center",
+  },
 });
